9abbb8c73fa58c91af528d7c68a0844454e4adaf
aaliaga <a.aliaga.val@gmail.com>
2017-05-15 14:01:41 -0400

    mismo error

--- js/main.js
+++ js/main.js
@@ -6,28 +6,28 @@ function validateForm(){
 	var pass = document.getElementById("input-password").value;
 	var tipo = document.getElementsByTagName("select")[0].selectedIndex;
 
-	if (nombre.length>0){
+	if (nombre.length==0){
 		var msnNombre = document.createElement("span");
 		msnNombre.innerHTML = "Debes escribir tu nombre";
 		document.getElementById("errorNombre").appendChild(msnNombre);
 		return false;
 	}
 
-	if (apellido.length>0){
+	if (apellido.length==0){
 		var msnApellido = document.createElement("span");
 		msnApellido.innerHTML = "Debes escribir tu apellido";
 		document.getElementById("errorApellido").appendChild(msnApellido);
 		return false;
 	}
 
-	if (correo.length>0){
+	if (correo.length==0){
 		var msnCorreo = document.createElement("span");
 		msnCorreo.innerHTML = "Debes escribir tu nombre";
 		document.getElementById("errorCorreo").appendChild(msnCorreo);
 		return false;
 	}
 
-	if (pass.length>0){
+	if (pass.length==0){
 		var msnPass = document.createElement("span");
 		msnPass.innerHTML = "Debes escribir tu nombre";
 		document.getElementById("errorPass").appendChild(msnPass);
